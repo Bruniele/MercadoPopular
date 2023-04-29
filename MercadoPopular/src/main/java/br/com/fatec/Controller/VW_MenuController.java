794c18fd27d6c6f8d823fb9328b6e16cdc864e83
Bruniele <brunieledias123@gmail.com>
2023-04-29 13:50:09 -0300

    Adicionando a tela de menu

--- MercadoPopular/src/main/java/br/com/fatec/Controller/VW_MenuController.java
+++ MercadoPopular/src/main/java/br/com/fatec/Controller/VW_MenuController.java
@@ -5,8 +5,15 @@
 package br.com.fatec.Controller;
 
 import java.net.URL;
+import br.com.fatec.*;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
+import javafx.scene.image.ImageView;
+import javafx.stage.Stage;
 
 /**
  * FXML Controller class
@@ -15,12 +22,52 @@ import javafx.fxml.Initializable;
  */
 public class VW_MenuController implements Initializable {
 
+    @FXML
+    private Button btnCliente;
+    @FXML
+    private Button btnProduto;
+    @FXML
+    private Button btnFornecedor;
+    @FXML
+    private Button btnPesquisar;
+
     /**
      * Initializes the controller class.
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
+        btnCliente.setGraphic(new ImageView("/br/com/fatec/icons/iconeMenuCliente.png"));
+        btnProduto.setGraphic(new ImageView("/br/com/fatec/icons/iconeMenuProduto.png"));
+        btnFornecedor.setGraphic(new ImageView("/br/com/fatec/icons/iconeMenuFornecedor.png"));
+        btnPesquisar.setGraphic(new ImageView("/br/com/fatec/icons/iconeMenuPesquisar.png"));
     }    
+
+    @FXML
+    private void btnCliente_Click(ActionEvent event) {
+    }
+
+    @FXML
+    private void btnProduto_Click(ActionEvent event) {
+        Produto open = new Produto();
+        try {
+            open.start(new Stage());
+            Stage stage = (Stage) btnProduto.getScene().getWindow();
+            stage.close();
+        } catch (Exception e) {
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setTitle("Erro!!");
+            alert.setContentText("Erro ao encontrar a pagina");
+            alert.show();
+        }
+    }
+
+    @FXML
+    private void btnFornecedor_Click(ActionEvent event) {
+    }
+
+    @FXML
+    private void btnPesquisar_Click(ActionEvent event) {
+    }
     
 }
--- MercadoPopular/src/main/java/br/com/fatec/Produto.java
+++ MercadoPopular/src/main/java/br/com/fatec/Produto.java
@@ -1,32 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
 package br.com.fatec;
 
+import java.io.IOException;
 import javafx.application.Application;
+import static javafx.application.Application.launch;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-import java.io.IOException;
-import javafx.scene.image.Image;
-
 /**
- * JavaFX App
+ *
+ * @author bruni
  */
-public class App extends Application {
+public class Produto extends Application {
 
     private static Scene scene;
 
     @Override
     public void start(Stage stage) throws IOException {
-        scene = new Scene(loadFXML("VW_Menu"));
-        
-        //Definindo o ícone na scene(cena)
-        Image aplicationIcon = new Image(getClass().getResourceAsStream("/br/com/fatec/image/mercadoPopular.png"));
-        stage.getIcons().add(aplicationIcon);
-        
-        //Não deixa maximizar a tela
-        stage.setResizable(false);
-        
+        scene = new Scene(loadFXML("VW_Produto"));
         stage.setScene(scene);
         stage.show();
     }
@@ -43,5 +39,4 @@ public class App extends Application {
     public static void main(String[] args) {
         launch();
     }
-
 }
