dcdc52e9a4c133fda9b86a7a825cd4fad54c097f
Bruniele <brunieledias123@gmail.com>
2023-05-27 10:41:16 -0300

    Adicionando a classe model do cliente

---
+++ MercadoPopular/src/main/java/br/com/fatec/model/Cliente.java
@@ -0,0 +1,97 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package br.com.fatec.model;
+
+/**
+ *
+ * @author bruni
+ */
+public class Cliente {
+    private int codigoCliente;
+    private String nome;
+    private String cpf;
+    private String telefone;
+    private String email;
+
+    public Cliente() {
+    }
+
+    public Cliente(int codigoCliente, String nome, String cpf, String telefone, String email) {
+        this.codigoCliente = codigoCliente;
+        this.nome = nome;
+        this.cpf = cpf;
+        this.telefone = telefone;
+        this.email = email;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 5;
+        hash = 53 * hash + this.codigoCliente;
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Cliente other = (Cliente) obj;
+        return this.codigoCliente == other.codigoCliente;
+    }
+
+    @Override
+    public String toString() {
+        return getNome();
+    }
+
+    //getters e setters
+    public int getCodigoCliente() {
+        return codigoCliente;
+    }
+
+    public void setCodigoCliente(int codigoCliente) {
+        this.codigoCliente = codigoCliente;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+       
+}
