75913fb1ffc91bb8be3bae3597d578437578ca1b
Bruniele <brunieledias123@gmail.com>
2023-05-27 15:18:51 -0300

    Inserindo o cliente na coleção

--- MercadoPopular/src/main/java/br/com/fatec/controller/VW_ClienteController.java
+++ MercadoPopular/src/main/java/br/com/fatec/controller/VW_ClienteController.java
@@ -4,14 +4,18 @@
  */
 package br.com.fatec.controller;
 
+import br.com.fatec.model.Cliente;
 import br.com.fatec.Menu;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
+import javafx.scene.control.ButtonType;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.TextField;
 import javafx.scene.image.ImageView;
@@ -45,7 +49,13 @@ public class VW_ClienteController implements Initializable {
     @FXML
     private TextField txtCodigoCliente;
     @FXML
-    private ComboBox<?> cbxCliente;
+    private ComboBox<Cliente> cbxCliente;
+    
+    //Variáveis auxiliares
+    //Coleção que conterá os dados para serem exibidos dentro
+    //da comboBox
+    ObservableList<Cliente> clientes =  
+        FXCollections.observableArrayList();
 
     /**
      * Initializes the controller class.
@@ -57,7 +67,25 @@ public class VW_ClienteController implements Initializable {
         btnExcluir.setGraphic(new ImageView("/br/com/fatec/icons/iconeExcluir.png"));
         btnAlterar.setGraphic(new ImageView("/br/com/fatec/icons/iconeAlterar.png"));
         btnPesquisar.setGraphic(new ImageView("/br/com/fatec/icons/iconePesquisar.png"));
+        
+        //colocar a coleção dentro da comboBox
+        cbxCliente.setItems(clientes);
+        
+        clientes.add(new Cliente(1, "Maria Teixeira de Souza", "11987564352", 
+            "54672178094", "mariateixeira@gmail.com"));
+        clientes.add(new Cliente(2, "João Siqueira Matos", "11987542318", 
+            "25670098341", "joaosiqueiramatos@gmail.com"));
+        
+        //Programa o evento de seleção na comboBox
+        eventoChangeValueCombo();
+        
     }    
+    
+    private void eventoChangeValueCombo() {
+        cbxCliente.valueProperty().addListener((ov, velho, novo) -> {
+            moveDadosModelTela(novo);
+        });
+    }   
 
     @FXML
     private void btnVoltar_Click(ActionEvent event) {
@@ -76,6 +104,32 @@ public class VW_ClienteController implements Initializable {
 
     @FXML
     private void btnInserir_Click(ActionEvent event) {
+        Cliente c = moveDadosTelaModel();
+        
+        //Para contains funcionar o equals() e hashCode()
+        //precisam ser programados em Cliente
+        if(clientes.contains(c)) {
+            Alert alerta = new Alert(Alert.AlertType.WARNING, 
+                           "Cliente duplicado, tente outro",
+                           ButtonType.OK);
+            alerta.showAndWait();
+            return;
+        }
+        else
+            clientes.add(c);
+        
+        //Limpa os campos
+        limpaCampos();
+        //Manda o cursor para o campo do código do cliente
+        txtCodigoCliente.requestFocus();
+    }
+    
+    private void limpaCampos() {
+        txtCodigoCliente.clear();
+        txtNome.clear();
+        txtCpf.clear();
+        txtTelefone.clear();
+        txtEmail.clear();
     }
 
     @FXML
@@ -88,6 +142,34 @@ public class VW_ClienteController implements Initializable {
 
     @FXML
     private void btnPesquisar_Click(ActionEvent event) {
+    }
+    
+    /**
+     * Move os dados que estão na tela para um objeto
+     * @return Objeto que receberá os dados
+     */
+    private Cliente moveDadosTelaModel() {
+        Cliente c = new Cliente();
+        c.setCodigoCliente(Integer.parseInt(txtCodigoCliente.getText()));
+        c.setNome(txtNome.getText());
+        c.setCpf(txtCpf.getText());
+        c.setTelefone(txtTelefone.getText());
+        c.setEmail(txtEmail.getText());
+
+        return c;
+    }
+    
+    /**
+     * Move os dados do objeto para a tela
+     * @param c objeto que contem os dados para exibição 
+     *          na tela
+     */
+    private void moveDadosModelTela(Cliente c) {
+        txtCodigoCliente.setText(Integer.toString(c.getCodigoCliente()));
+        txtNome.setText(c.getNome());
+        txtCpf.setText(c.getCpf());
+        txtTelefone.setText(c.getTelefone());
+        txtEmail.setText(c.getEmail());
     }
     
 }
